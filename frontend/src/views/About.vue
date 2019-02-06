913145c424df2b7c772d1c3ceb366edd69d0d63c
Antoine Lupiac <antoine.lupiac@etu.unice.fr>
2019-02-06 09:34:21 +0100

    FRONTEND: changes in About page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,11 +1,105 @@
 <template>
   <div class="about has-nav">
-    <h1>This is an about page</h1>
+    <h1 class="is-size-3-desktop is-size-4-touch">This project has been designed, developed and tested by:</h1>
+
+    <!--<div class="columns is-centered">-->
+    <!--<div class="column is-6 is-centered box">-->
+
+    <!--<div class="card">-->
+    <!--<div class="card-content">-->
+    <!--<p class="title">-->
+    <!--DEGAND Sébastien-->
+    <!--</p>-->
+    <!--<p class="subtitle">-->
+    <!--Jeff Atwood-->
+    <!--</p>-->
+    <!--</div>-->
+    <!--<footer class="card-footer">-->
+    <!--<p class="card-footer-item">-->
+    <!--<span>-->
+    <!--View on <a href="https://github.com/SebastienDegand">Github</a>-->
+    <!--</span>-->
+    <!--</p>-->
+    <!--<p class="card-footer-item">-->
+    <!--<span>-->
+    <!--Share on <a href="#">Facebook</a>-->
+    <!--</span>-->
+    <!--</p>-->
+    <!--</footer>-->
+    <!--</div>-->
+    <!--</div>-->
+
+    <!--<div class="column is-6 is-centered box">-->
+    <!--<div class="card">-->
+    <!--<div class="card-content">-->
+    <!--<p class="title">-->
+    <!--“There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”-->
+    <!--</p>-->
+    <!--<p class="subtitle">-->
+    <!--Jeff Atwood-->
+    <!--</p>-->
+    <!--</div>-->
+    <!--<footer class="card-footer">-->
+    <!--<p class="card-footer-item">-->
+    <!--<span>-->
+    <!--View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>-->
+    <!--</span>-->
+    <!--</p>-->
+    <!--<p class="card-footer-item">-->
+    <!--<span>-->
+    <!--Share on <a href="#">Facebook</a>-->
+    <!--</span>-->
+    <!--</p>-->
+    <!--</footer>-->
+    <!--</div>-->
+
+    <!--</div>-->
+    <!--</div>-->
+
+    <div class="columns is-centered is-size-5-desktop is-size-6-touch" style="padding-top: 3%">
+      <div class="column is-4 is-centered box my-card" style="margin-right: 12%">
+        <h1><strong>DEGAND Sébastien:</strong></h1>
+        <div>
+          <p>
+            <i class="far fa-envelope"></i>
+            sebastien.degand@etu.unice.fr
+          </p>
+          <p>
+            <i class="fab fa-github"></i>
+            Find on <a href="https://github.com/SebastienDegand">Github</a> </p>
+        </div>
+      </div>
+      <div class="column is-3 is-centered box my-card">
+        <h1><strong>LUPIAC Antoine:</strong></h1>
+        <div>
+          <p>
+            <i class="far fa-envelope"></i>
+            antoine.lupiac0@gmail.com
+          </p>
+          <p>
+            <i class="fab fa-github"></i>
+            Find on <a href="https://github.com/Lupiac">Github</a>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <h1>Under the supervision of Oscar RODRÍGUEZ ROCHA.</h1>
   </div>
 </template>
 
 <style scoped>
-.has-nav{
-  margin-top: 5%;
-}
+  .has-nav{
+    margin-top: 10vh;
+  }
+  .my-card{
+    height: 15%;
+    width: 33%;
+  }
+
+  .my-card div{
+    text-align: left;
+    padding: 5%;
+  }
+
 </style>
